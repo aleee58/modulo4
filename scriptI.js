7b40b5f821d3a081ce79158b0b0cdeac7ced8c6d
alejandra <aleee5858@gmail.com>
2024-02-21 20:05:48 -0300

    first commit

---
+++ scriptI.js
@@ -0,0 +1,11 @@
+function saludar(){
+    console.log()
+}
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
