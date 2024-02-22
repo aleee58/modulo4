e3e13a9f51adec91b394d34038d664f317ccf3f9
alejandra <aleee5858@gmail.com>
2024-02-21 21:34:37 -0300

    faltaAun

--- scriptI.js
+++ scriptI.js
@@ -1,11 +1,21 @@
-function saludar(){
-    console.log()
+let preguntas = [];
+let misRespuestas = Array.from8[{legth:10}];
+
+document.addEventListener("DOMContentLoaded", function(){
+  let token =sessionStorage.getItem("token");
+     if (token) {
+      console.log("token encontrado: ", token)
+     } else {
+      generarToken()
+     }
+
+});
+
+function desordenar() {
+  return Math.random() - 0,5
+  };
+
+const generarToken = () => {
+  fetch("https://opentdb.com/api_token.php?command-request")
+
 }
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.listen(3000)
